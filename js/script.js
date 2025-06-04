383abb153d6ea78fa1a11f6919d4ee0304ac6318
Prefna Vagheni <prefnavagheni54@gmail.com>
2025-06-05 00:41:06 +0200

    Improved version

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const date = new Date();
-const hour = date.getHours();
+const hour = `${date.getHours()}`.padStart(2, 0);
 const min = `${date.getMinutes()}`.padStart(2, 0);
 
 document.querySelector('.hour').textContent = hour;
