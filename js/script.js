691831bf63eaf6884a58b03de37ee867caf825d3
Prefna Vagheni <prefnavagheni54@gmail.com>
2025-06-05 23:49:11 +0200

    added the api, but still requires work

--- js/script.js
+++ js/script.js
@@ -6,3 +6,12 @@ const min = `${date.getMinutes()}`.padStart(2, 0);
 
 document.querySelector('.hour').textContent = hour;
 document.querySelector('.min').textContent = min;
+
+fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=${APY_KEY}`)
+  .then((res) => {
+    if (!res.ok) throw new Error('jfdjs');
+    console.log(res);
+    return res.json();
+  })
+  .then((data) => console.log(data))
+  .catch((err) => console.error(err.message));
