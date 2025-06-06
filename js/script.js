e29269c65885637cd8877961c8867fd780177c7d
Prefna Vagheni <prefnavagheni54@gmail.com>
2025-06-06 21:27:49 +0200

    Appended Data to the DOM

--- js/script.js
+++ js/script.js
@@ -5,15 +5,38 @@
 const date = new Date();
 const hour = `${date.getHours()}`.padStart(2, 0);
 const min = `${date.getMinutes()}`.padStart(2, 0);
+const moviesContainer = document.querySelector('.list--container');
 
 document.querySelector('.hour').textContent = hour;
 document.querySelector('.min').textContent = min;
 
-fetch(`https://api.tvmaze.com/search/shows?q=breaking+bad`)
+fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=${API}`)
   .then((res) => {
     if (!res.ok) throw new Error('jfdjs');
     console.log(res);
     return res.json();
   })
-  .then((data) => console.log(data))
+  .then((data) => {
+    const moviesArray = data.results;
+    const releaseDate = moviesArray[0].release_date.slice(0, 4);
+    console.log(releaseDate);
+    moviesArray.forEach((el) => {
+      const html = `
+            <article class="trending--article">
+              <article class="journal"></article>
+              <h4 class="trending--title">${el.original_title}</h4>
+              <p class="trending--description opacity--low">
+                <span class="prod--year">${el.release_date.slice(
+                  0,
+                  4
+                )}</span>&nbsp;
+                <span class="type">Drama</span> &nbsp;
+                <span class="movie--duration">1h 30m</span>
+              </p>
+            </article>
+            `;
+      moviesContainer.insertAdjacentHTML('beforeend', html);
+    });
+    console.log(moviesArray);
+  })
   .catch((err) => console.error(err.message));
